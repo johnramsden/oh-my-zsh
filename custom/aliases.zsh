1bf07a1ad1d78a431b92cc7d7d22f0e7b8a74e59
John Ramsden <johnramsden@users.noreply.github.com>
2017-07-11 19:27:19 -0700

    Add custom files and zlogin

---
+++ custom/aliases.zsh
@@ -0,0 +1,48 @@
+# Custom aliases
+
+# root commands
+alias suno='sudo nano'
+alias smi='sudo micro'
+
+alias mi='sudo micro'
+
+alias screenshot="import -window root ~/Pictures/$(date '+%Y%m%d-%H%M%S').png"
+
+# Set grub to reboot to windows, update and reboot
+#alias nextboot='sudo replaceboot'
+
+# Boot windows
+#alias winboot='nextboot hector && reboot'
+
+# System
+alias timersd='systemctl list-timers | less'
+
+# Misc
+alias efstab='sudo micro /etc/fstab'
+
+# tar
+# Uncompressed tar archive: $ tar -cvf archive_name.tar dirname/
+alias tarun='tar -cvf'
+# Creating a tar gzipped compressed archive: $ tar -cvzf archive_name.tar.gz dirname/
+alias gtar='tar -cvzf'
+# Creating a bzipped tar compressed more than gzip archive: $ tar -cvfj archive_name.tar.bz2 dirname/
+alias btar='tar -cvfj'
+# Extract a *.tar file using option xvf: $ tar xvf archive_name.tar
+alias xtar='tar -xvf'
+# Extract a gzipped tar archive ( *.tar.gz ) using option xvzf: $ tar xvfz archive_name.tar.gz
+alias gxtar='tar -xvfz'
+# Extracting a bzipped tar archive ( *.tar.bz2 ) using option xvjf: $ tar xvfj archive_name.tar.bz2
+alias bxtar='tar -xvfj'
+
+alias sshscan="sudo nmap -p 22 --open -sV  192.168.0.0/24"
+
+alias startvncserv="systemctl --user start vncserver@:1"
+
+alias unitlist="systemctl list-units --type service --state active"
+
+# Pacman.conf
+
+alias paced="sudo micro /etc/pacman.conf"
+
+# Dotfile management
+alias config='/usr/bin/git --git-dir=$HOME/.config/dotfiles/ --work-tree=$HOME'
---
+++ custom/plugins/zfs-snap-pacman/zfs-snap-pacman.plugin.zsh
@@ -0,0 +1,64 @@
+datasets=('vault/ROOT/default' 'vault/usr' 'vault/var' 'shard/home')
+
+iterate-snaps(){
+  for dataset in ${datasets}; do
+    echo "Taking snapshot: ${dataset}@${1}"
+    sudo zfs snapshot "${dataset}@${1}"
+  done
+}
+
+# ZFS Snapshot before command
+# Update with pre and post snapshot
+zupg(){
+  snap_date="$(date +%Y-%m-%d-%H%M%S)"
+  echo "Updating system..."
+
+  iterate-snaps "pre-update-${snap_date}"
+
+  echo "Starting update..."
+  pacaur -Syu
+
+  iterate-snaps "post-update-${snap_date}"
+}
+
+zin(){
+  packages=""
+  snap_date="$(date +%Y-%m-%d-%H%M%S)"
+  for var in $@;
+    do
+      packages="${packages}-${var}";
+  done
+  echo "Pre-install..."
+
+  iterate-snaps "pre-install${packages}-${snap_date}"
+
+  echo
+  echo "Installing packages: ${@}..."
+  sudo pacman -S ${@}
+  echo
+
+  echo "Post-install..."
+
+  iterate-snaps "post-install${packages}-${snap_date}"
+}
+
+zain(){
+  packages=""
+  snap_date="$(date +%Y-%m-%d-%H%M%S)"
+  for var in $@;
+    do
+      packages="${packages}-${var}";
+  done
+  echo "Pre-install..."
+
+  iterate-snaps "pre-install${packages}-${snap_date}"
+
+  echo
+  echo "Installing packages: ${@}..."
+  pacaur -S ${@}
+  echo
+
+  echo "Post-install..."
+
+  iterate-snaps "post-install${packages}-${snap_date}"
+}
