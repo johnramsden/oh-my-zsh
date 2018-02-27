014ab19bf146eff07130be8322517075fa6ee475
John Ramsden <johnramsden@users.noreply.github.com>
2018-02-27 11:34:51 -0800

    Add datasets to settings

--- .config/oh-my-zsh/custom/plugins/zsnapac/zsnapac-settings.zsh
+++ .config/oh-my-zsh/custom/plugins/zsnapac/zsnapac-settings.zsh
@@ -1,5 +1,8 @@
 # Add datasets to snapshot
-ZFS_PAC_SNAP_DATASETS=("vault/sys/$(hostname)/ROOT/default")
+ZFS_PAC_SNAP_DATASETS=( "vault/sys/$(hostname)/ROOT/default"
+                        "vault/sys/$(hostname)/var/cache"
+                        "vault/sys/$(hostname)/var/cache/pacman"
+                        "vault/sys/$(hostname)/var/cache/pacman/custom")
 
 # Replace function with your aur update of choice
 ZFS_AUR_UPDATE(){
